509e2e0708024c3f7367af0a59cbe80464d97322
ashishkumar9608 <101400774+ashishkumar9608@users.noreply.github.com>
2024-01-22 15:23:41 +0000

    container config done

---
+++ basic_01/DataTypeSummary.js
@@ -0,0 +1,16 @@
+// primitive 
+// 7 type : string , number, boolean, null , undefined, symbol, BigInt
+
+// Reference (Non Primitive)
+// Array, objects, Functions
+
+const arr = ["ashish","name"]
+
+let myobj = {
+    name:"ashish",
+    age:22,
+}
+
+const myfunction = function(){
+    console.log("Hello Ashish");
+}
