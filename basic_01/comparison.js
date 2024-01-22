642610357f59569a141e5a4c44a32240f3b743ab
ashishkumar9608 <101400774+ashishkumar9608@users.noreply.github.com>
2024-01-22 15:11:52 +0000

    container config done

---
+++ basic_01/comparison.js
@@ -0,0 +1,7 @@
+ // === strict check they check for datatype
+ // == change to datatype for checking 
+
+ console.log(null > 0);  // false
+ console.log(null == 0); // false
+ console.log(null >= 0); // true
+ console.log(null === 0); // false
