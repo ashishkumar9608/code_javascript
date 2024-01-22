379b4b57b5ca99449a6978490c7d38c5b6b49bd8
ashishkumar9608 <101400774+ashishkumar9608@users.noreply.github.com>
2024-01-22 14:47:51 +0000

    container config done

---
+++ basic_01/conversionOperation.js
@@ -0,0 +1,36 @@
+// let score = "33";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+
+// NaN Not a Number
+// "33" => 33
+// "33abc" => NaN
+// true => 1 
+
+// "" => false
+// "ashish" =>true
+
+
+
+// ******************* Operation ********************
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+// 2+2 = add;
+// 2-2 = subtract;
+// 2*2 = Multiply;
+// 2**2 = power;
+// 2/2 = divide;
+// 2%2 = remainder;
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log(1 + 1 + "3") // 23;
+console.log("1" + 1 + 4); // 114
